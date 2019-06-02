2a18ec225eaed5e4f37b3a317df8a1fabd723792
Nayef Sayar <nayefisaac@gmail.com>
2019-06-02 18:56:46 -0400

    second commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var thumbNails = [
+    {
+    thumbNailImage: "images/queen-of-hearts.png"
+    },
+    {
+    cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+    cardImage: "images/king-of-hearts.png"
+    }
+    ];
